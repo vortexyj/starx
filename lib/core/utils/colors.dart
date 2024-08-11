5717443357c1e0f02bb3ca57d450ff0060c14853
vortexyj <yossefjehad@gmail.com>
2024-08-11 20:16:47 +0300

    finish fonts and colors

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorStyle {
+  static const black = Color(000000);
+  static const gray = Color(0x4E4E4E);
+  static const lightGray = Color(0xC4C4C4);
+  static const white = Color(0xFFFFFF);
+  static const accentStroke = Color(0x25A0B0);
+  static const mainThemeColor = Color(0xE1F6F4);
+  static const pink = Color(0xF5C8C6);
+  static const accentLines = Color(0xEEF2E2);
+}
---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+
+abstract class Styles {
+  static const title39B = TextStyle(
+    fontSize: 39,
+    fontWeight: FontWeight.bold,
+  );
+  static const title31B = TextStyle(
+    fontSize: 31,
+    fontWeight: FontWeight.bold,
+  );
+  static const title25B = TextStyle(
+    fontSize: 25,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 1.2,
+  );
+  static const title25 = TextStyle(
+    fontSize: 25,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const title20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+  );
+  static const body16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+  );
+  static const body14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+  static const body12Gray = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.normal,
+    color: Color(656565),
+  );
+}
+
+const TextStyle textStyle = TextStyle();
